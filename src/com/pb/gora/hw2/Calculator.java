6cf874040848688a214705a053010d25dd6b5eed
Stas Gora <gorasj190689@gmail.com>
2021-10-14 23:10:21 +0300

    init commit

--- src/com/pb/gora/hw2/Calculator.java
+++ src/com/pb/gora/hw2/Calculator.java
@@ -2,7 +2,7 @@ package com.pb.gora.hw2;
 
 import java.util.Scanner;
 
-public class Calculator {
+public class Calculator{
  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     int operand1;
